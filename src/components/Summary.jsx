634748b4287296fbaba745d6960d6a0248e6d6db
Thaw Zin Tun <thawzintun98@gmail.com>
2023-05-25 17:30:22 +0630

    a

---
+++ src/components/Summary.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Summary = () => {
+    return (
+        <section className="summary">
+            <h2>Best Shopping Website in Myanmar</h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Voluptate amet reiciendis, natus voluptates repellendus nostrum,
+                iure eligendi deserunt quam provident similique asperiores
+                temporibus totam accusantium dolore odio sunt dolor vel.
+            </p>
+        </section>
+    );
+};
+
+export default Summary;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+
+const Main = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+        </>
+    );
+};
+
+export default Main;
